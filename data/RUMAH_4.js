713f0a11ad5d6e703b945e218bc7a93c345ca604
adeha14 <157741572+adeha14@users.noreply.github.com>
2024-01-26 01:54:35 +0700

    Add files via upload

---
+++ data/RUMAH_4.js
@@ -0,0 +1,1 @@
+var json_RUMAH_4 = {"type":"FeatureCollection","name":"RUMAH_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1","RUMAH":"RUMAH ADEHA"},"geometry":{"type":"Point","coordinates":[101.633679419567017,-0.484095542653562]}}]}
